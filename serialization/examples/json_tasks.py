0a37135067c694e769c414df1ed14ee8affc3970
silox <edruisl99@gmail.com>
2024-07-08 20:34:31 +0200

    Add json examples

---
+++ serialization/examples/json_tasks.py
@@ -0,0 +1,33 @@
+# Task 1
+# Napis funkciu load_book_from_json(file_name), ktora nacita knihy do dataclassy
+# Book (ktoru treba tiez vytvorit)
+# Format jsonu:
+"""
+{
+    "books": [
+        {
+            "title": "To Kill a Mockingbird",
+            "author": "Harper Lee",
+            "year": 1960,
+            "isbn": "978-0061120084"
+        },
+        {
+            "title": "1984",
+            "author": "George Orwell",
+            "year": 1949,
+            "isbn": "978-0451524935"
+        }
+    ]
+}
+"""
+
+# Task 2
+# Napis funkciu print_books(books), ktora v peknom formate vypise vsetky knihy
+# a na zaver vypise aj ich pocet
+
+# Task 3
+# Napis funkciu add_book(book), ktora upravi json file `books.json` tak, ze sa don
+# prida nova kniha predana cez parameter `book` (typu `Book`)
+
+# Task 4
+# Napis funkciu remove_book(isbn), ktora vymaze knihu z .json suboru podla isbn cisla.
